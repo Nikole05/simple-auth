7bb8ef6107a151eedadeabee9a6178c9cc450b02
Nico <nico@Nicos-MacBook-Pro.local>
2019-01-02 23:15:37 +0200

    add textInput

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 
@@ -7,8 +7,12 @@ class LoginForm extends Component {
     render() {
         return (
            <Card>
+                <CardSection>
+                    <TextInput style={{ height: 20, width: 100 }} />
+                </CardSection>
+
                 <CardSection />
-                <CardSection />
+
                 <CardSection>
                     <Button>
                         Log in
