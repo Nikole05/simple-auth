a623bb9ec78d9ae3823f95e35e1be372ae09d9a9
Nico <nico@Nicos-MacBook-Pro.local>
2019-01-02 22:56:17 +0200

    add cpmponents and firebase.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+                apiKey: "AIzaSyDeCegA4JREJSlW-APNOym0on6ZE7HvBvo",
+                authDomain: "auth-simple.firebaseapp.com",
+                databaseURL: "https://auth-simple.firebaseio.com",
+                projectId: "auth-simple",
+                storageBucket: "auth-simple.appspot.com",
+                messagingSenderId: "139302578518"
+        });
+    }
+
     render() {
         return (
             <View>
-                <Text>An app</Text>
+                <Header headerText="Authentication" />
+                <Text>An app!</Text>
             </View>
         );
     }
